c5f05afec48a6b947a50e4e1856921a9fdd7d735
妖雨纯 <876451336@qq.com>
2026-08-26 09:28:49 +0800

    refactor(nacos): inject complete runtime options

--- src/runtime/nacos/nacos.interface.ts
+++ src/runtime/nacos/nacos.interface.ts
@@ -1,6 +1,17 @@
 export interface NacosRuntimeOptions {
+    serverAddr: string
+    namespace: string
+    username?: string
+    password?: string
+    requestTimeout: number
+    configDataId: string
+    configGroup: string
+    registerEnabled: boolean
+    registerRequired: boolean
     serviceName: string
-    defaultPort: number
+    discoveryGroup: string
+    registerIp?: string
+    registerPort: number
 }
 
 export const NACOS_RUNTIME_OPTIONS = Symbol('NACOS_RUNTIME_OPTIONS')
